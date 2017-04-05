d33b84f45760d69780aae4ceca56ded4849300fe
CANIS ALISE p1616229 <p1616229@univ-lyon1.fr>
2017-04-05 16:57:13 +0200

    avancement case

---
+++ BatailleNavale/case.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatailleNavale
+{
+    class Case
+    {
+        int x;
+        int y;
+        Bateau b;
+
+        public Case()
+        {
+        }
+
+        public static bool isOccupated(){
+            return false;
+        }
+    }
+}
